d84da13764aac5b55f220cd89a60e5ffe94abc15
Asrar2244 <mohammednooruddinasrar66@gmail.com>
2024-11-02 06:55:27 +0100

    cors changed

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json()); // Middleware for parsing JSON
 app.use(cors({
-    origin: 'https://mbslogisticsind.github.io/logistics/', // Adjust to specific domains if necessary
+    origin: '*', // Adjust to specific domains if necessary
     methods: ['GET', 'POST', 'PUT', 'DELETE'] // Allows all relevant methods
 }));
 
